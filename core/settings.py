020ec5ad1b706104574d472aa8ee55320fc084a5
aslanandreia7-sudo <aslanadreia7@gmail.com>
2026-02-28 00:56:41 -0800

    Configuración final de STATIC_ROOT para WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -118,5 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Esta carpeta es donde WhiteNoise buscará los archivos en el servidor
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Esto ayuda a que los archivos se compriman y carguen más rápido
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
